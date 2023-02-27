9a8f62b61d17c4a512c3d287305c199a0e0d2112
SHAJLP <sophiaaltaf73@gmail.com>
2023-02-27 21:10:03 +0000

    amendments to file

--- controllers/api/meeting-routes.js
+++ controllers/api/meeting-routes.js
@@ -1,26 +1,23 @@
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    
-    socket.on('join', (roomId) => {
-      socket.join(roomId);
-      console.log(`User joined room ${roomId}`);
-    });
-    
-    socket.on('message', (roomId, message) => {
-      socket.to(roomId).emit('message', message);
-    });
-    
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname + "/api/meeting-routes.js"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+    console.log(`User joined room ${roomId}`);
   });
 
+  socket.on("message", (roomId, message) => {
+    socket.to(roomId).emit("message", message);
+  });
 
-
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-
-app.use(express.static(__dirname + '/api/meeting-routes.js'));
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
